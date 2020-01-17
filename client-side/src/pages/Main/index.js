327ce12bd4c778452452fbf570393788f3ead0ed
Pedro Henrico <pedrohromania@gmail.com>
2020-01-17 00:29:10 -0300

    Updated to React Hooks syntax

--- client-side/src/pages/Main/index.js
+++ client-side/src/pages/Main/index.js
@@ -1,68 +1,63 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import api from '../../services/api'
 import './styles.css'
 
-export default class App extends Component {
+export default function App(props) {
 
-    state = {
-        pokemons: [],
-        value: "",
-    }
+    const [pokemons, setPokemons] = useState([]);
+    const [value, setValue] = useState('');
 
-    componentDidMount() {
-        this.getPokemons();
-    }
+    useEffect(() => {
+        getPokemons();
+    }, []);
 
-    getPokemons = async (offset = 1) => {
+    async function getPokemons(offset = 1) {
         const result = await api.get(`/${offset}`);
-        this.setState({ pokemons: result.data });
+        setPokemons(result.data);
     }
 
-    nextPokemons = () => {
-        const offset = this.state.pokemons[this.state.pokemons.length - 1].id;
-        this.getPokemons(offset);
+    function nextPokemons() {
+        const offset = pokemons[pokemons.length - 1].id;
+        getPokemons(offset);
     }
 
 
-    backPokemons = () => {
-        const offset = this.state.pokemons[0].id;
+    function backPokemons() {
+        const offset = pokemons[0].id;
 
         if (offset - 20 < 1)
-            this.getPokemons(1);
+            getPokemons(1);
         else
-            this.getPokemons(offset - 20);
+            getPokemons(offset - 20);
     }
 
-    HandleSubmit = (event) => {
+    function HandleSubmit(event) {
         event.preventDefault();
-        const value = this.state.value;
 
         if (value.trim())
-            this.props.history.push(`/pokemon/${value}`);
+            props.history.push(`/pokemon/${value}`);
     }
 
-    HandleChange = (event) => {
-        this.setState({ value: event.target.value });
+    function HandleChange(event) {
+        setValue(event.target.value)
     }
 
-    render() {
-        return <div>
-            <form onSubmit={this.HandleSubmit}>
-                <input type="text" id="search" value={this.state.value} onChange={this.HandleChange} placeholder='ID or name' />
-                <input type="submit" value="Search" id="SearchBtn" />
-            </form>
-            <ul id="pokemon-list">
-                {this.state.pokemons.map(pokemon => (
-                    <Link key={pokemon.id} id="pokemon-li" to={`/pokemon/${pokemon.id}`}>
-                        <span><h3>#{pokemon.id} {pokemon.name.toUpperCase()}</h3></span>
-                        <img src={pokemon.image} alt={pokemon.name} />
-                    </Link>
-                ))}
-            </ul>
-            <button id="showMore" onClick={this.backPokemons}>Previous</button>
-            <button id="showMore" onClick={this.nextPokemons}>Next</button>
+    return <div>
+        <form onSubmit={(event) => HandleSubmit(event)}>
+            <input type="text" id="search" value={value} onChange={(event) => HandleChange(event)} placeholder='ID or name' />
+            <input type="submit" value="Search" id="SearchBtn" />
+        </form>
+        <ul id="pokemon-list">
+            {pokemons.map(pokemon => (
+                <Link key={pokemon.id} id="pokemon-li" to={`/pokemon/${pokemon.id}`}>
+                    <span><h3>#{pokemon.id} {pokemon.name.toUpperCase()}</h3></span>
+                    <img src={pokemon.image} alt={pokemon.name} />
+                </Link>
+            ))}
+        </ul>
+        <button id="showMore" onClick={() => backPokemons()}>Previous</button>
+        <button id="showMore" onClick={() => nextPokemons()}>Next</button>
 
-        </div>
-    }
+    </div>
 }
