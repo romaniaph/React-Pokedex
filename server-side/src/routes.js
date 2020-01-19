70c285b4a6f5be2c4897c8a31e75e7420956b4ad
Pedro Henrico <pedrohromania@gmail.com>
2020-01-19 16:20:51 -0300

    Updated folders and files

--- server-side/src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const routes = express.Router();
-const pokedex = require('./controllers/pokedex');
-
-routes.get('/:offset', pokedex.getPokemons);
-routes.get('/pokemon/:pokemon', pokedex.getPokemon);
-
-module.exports = routes;
